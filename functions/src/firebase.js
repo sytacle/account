22a3176e28fee9d5e44efd705699abcc95cf7092
Rex Fernandez Luciano <rexluciano@yahoo.com>
2026-09-14 17:24:48 +0800

    some code changes

--- functions/src/firebase.js
+++
@@ -1,7 +0,0 @@
-import { getApps, initializeApp } from "firebase-admin/app";
-import { getAuth } from "firebase-admin/auth";
-import { getFirestore, Timestamp, FieldValue } from "firebase-admin/firestore";
-if (!getApps().length) initializeApp();
-export const auth = getAuth();
-export const db = getFirestore();
-export { Timestamp, FieldValue };
